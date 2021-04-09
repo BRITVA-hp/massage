cb3066906dfe0fd5e48b646f2fbd69dfe6a64dac
BRITVA-hp <borbritvahp@gmail.com>
2021-04-10 00:49:17 +0400

    modal and qeustMain

--- js/script.js
+++ js/script.js
@@ -5,6 +5,57 @@ window.addEventListener('DOMContentLoaded', () => {
           headerMenuClose = document.querySelector('.header__menu__close');
 
 
+    // Функция для появления-скрытия модалки
+
+    function calcScroll() {
+        let div = document.createElement('div');
+      
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+      
+        document.body.appendChild(div);
+        let scarollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+      
+        return scarollWidth;
+      }
+
+    let scrollWidth = calcScroll();
+
+    function modal(modal, modalActiveClass, triggers, modalClose) {
+        const triggers_ = document.querySelectorAll(triggers),
+              modal_ = document.querySelector(modal),
+              modalClose_ = document.querySelector(modalClose);
+
+        if (triggers_.length > 0) {
+            triggers_.forEach(item => {
+                item.addEventListener('click', () => {
+                    modal_.classList.add(modalActiveClass);
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+                });
+            });
+
+            modalClose_.addEventListener('click', () => {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            });
+    
+            modal_.addEventListener('click', (e) => {
+                if (e.target.classList.contains(modal.replace(/\./, ''))) {
+                    modal_.classList.remove(modalActiveClass);
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = '0px';
+                }
+            });
+        }
+    }
+
+    modal('.modal', 'modal--visible', '[data-modal]', '.modal__close');
+
     // появление - исчезновение меню на мобилке при нажатии на бургер
     function headerDeleteClass() {
         header.classList.remove('header--absolute');
